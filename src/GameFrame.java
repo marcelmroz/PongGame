02a930936cd752777381a5ec06256917f25efda2
Marcel Mroz <mmroz.marcel@gmail.com>
2023-09-21 13:46:48 +0200

    create classes and methods

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,6 +4,7 @@ import java.util.*;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
+    GamePanel panel = new GamePanel();
 
     GameFrame(){
 
--- src/PongGame.java
+++ src/PongGame.java
@@ -5,6 +5,6 @@ import javax.swing.*;
 
 public class PongGame {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GameFrame frame = new GameFrame();
     }
 }
