980d7ff66f2f9dce70f678083560d726511209fb
Marcel Mroz <mmroz.marcel@gmail.com>
2023-09-26 11:27:00 +0200

    ball bounces of the paddles

--- src/Ball.java
+++ src/Ball.java
@@ -1,12 +1,11 @@
 import java.awt.*;
-import java.awt.event.*;
 import java.util.*;
-import javax.swing.*;
 
 public class Ball extends Rectangle {
     Random random;
     int xVelocity;
     int yVelocity;
+    int initialSpeed = 3;
 
     Ball(int x, int y, int width, int height){
         super(x,y,width,height);
@@ -15,13 +14,13 @@ public class Ball extends Rectangle {
         if(randomXDirection == 0){
             randomXDirection--;
         }
-        setXDirection(randomXDirection);
+        setXDirection(randomXDirection*initialSpeed);
 
         int randomYDirection = random.nextInt(2);
         if(randomYDirection == 0){
             randomYDirection--;
         }
-        setYDirection(randomYDirection);
+        setYDirection(randomYDirection*initialSpeed);
     }
     public void setXDirection(int randomXDirection) {
         xVelocity = randomXDirection;
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -60,7 +60,38 @@ public class GamePanel extends JPanel implements Runnable{
         ball.move();
     }
     public void checkCollision(){
-        //borders
+        //ball border
+        if(ball.y <= 0){
+            ball.setYDirection(-ball.yVelocity);
+        }
+        if(ball.y >= GAME_HEIGHT - BALL_DIAMETER){
+            ball.setYDirection(-ball.yVelocity);
+        }
+        //ball and paddle
+        if(ball.intersects(paddle1)){
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity += 0.5;
+            if(ball.yVelocity > 0) {
+                ball.yVelocity += 0.5;
+            }else {
+                ball.yVelocity--;
+            }
+            ball.setXDirection(ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+        if(ball.intersects(paddle2)){
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity += 0.5;
+            if(ball.yVelocity > 0) {
+                ball.yVelocity += 0.5;
+            }else {
+                ball.yVelocity--;
+            }
+            ball.setXDirection(-ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+
+        //paddles borders
         if(paddle1.y <= 0){
             paddle1.y = 0;
         }
